d7e2f4afa856f0095dd47fac4d9aac9762d60739
ChristianAlameda <jazzbilbo@gmail.com>
2023-10-29 16:03:49 -0700

    keep making updates

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from database import Database
 
 class MyFlaskApp:
     def __init__(self):
-        self.app = Flask(__name__, template_folder='templates', static_folder='static')
-        
-        #HOMEPAGE
+        # Initialize Flask app
+        self.app = Flask(__name__)
+
+        # Connect to the database
+        self.databaseClass = Database()
+        self.databaseClass.connect()
+
+        # Add routes to the app
         self.app.add_url_rule('/', 'index', self.index)
-        
+        self.app.add_url_rule('/add', 'add', self.add)
+        self.app.add_url_rule('/rem', 'rem', self.rem)
+        self.app.add_url_rule('/view', 'view', self.view)
 
     def index(self):
-        return render_template('homepage.html')
+        return render_template('home.html')
 
+    def add(self):
+        return render_template('add.html')
 
-def startFlask(requestQ, dataQ):
-    newFlask = MyFlaskApp(requestQ, dataQ)
-    newFlask.run()
+    def rem(self):
+        return render_template('rem.html')
 
-# Usage
-if __name__ == '__main__':
-    request_queue = Queue()
-    data_queue = Queue()
-    my_flask_app = MyFlaskApp(request_queue, data_queue)
-    my_flask_app.run()
+    def view(self):
+        allPosts = self.databaseClass.getPosts({})
+        return render_template('view.html', allPosts=allPosts)
+
+if __name__ == "__main__":
+    x = MyFlaskApp()
+    x.app.run(host="0.0.0.0", port=7777)

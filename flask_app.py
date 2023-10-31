ee3617ac7308a8c6db2f589192999c189dfadfac
ChristianAlameda <jazzbilbo@gmail.com>
2023-10-31 12:00:09 -0700

    was able to test out how to make multiple forms from user input

--- flask_app.py
+++ flask_app.py
@@ -1,70 +1,24 @@
-import random
-
-from flask import Flask, request, render_template
-from pymongo import MongoClient
-import pymongo
-
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-# Set up MongoDB connection
-client = MongoClient('mongodb://localhost:27017/')
-db = client['Avilog_ALPHA']
-collection = db['users']
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-html_form = """<!DOCTYPE html>
-<html>
-<head>
-    <title>User Registration Form</title>
-</head>
-<body>
-    <h1>User Registration Form</h1>
-    <form name="registrationForm" action="/submit" method="post">
-    
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br>
+@app.route('/add')
+def add():
+    return render_template('som.html')
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
+@app.route('/add/new_added_document', methods=['POST'])
+def add1():
+    data = request.json  # Access the JSON data sent in the POST request
 
-        <!-- Add more input fields for other data -->
-        
-        <input type="submit" value="Submit">
-        <input type="reset" value="Reset">
-    </form>
-</body>
+    # Process and store the data as needed
+    # For demonstration, we'll simply print the data to the console
+    print(data)
 
-
-</html>"""
-
-@app.route('/registration', methods=['GET'])
-def show_registration_form():
-    return html_form
-
-@app.route('/submit', methods=['POST'])
-def submit_form():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-
-        #_id = generate_user_id()
-        
-        # Insert the data into MongoDB
-        user_data = {
-            #"_id": _id,
-            "username": username,
-            "email": email
-        }
-        collection.insert_one(user_data)
-        #collection.create_index([("user_id", pymongo.ASCENDING)], unique=True)
-
-        return "Data submitted successfully."
-
-
-def generate_user_id():
-    while True:
-        user_id = f'BW{random.randint(1, 99999999):08}'
-        return user_id
+    return jsonify({'message': 'Data received successfully'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
